3a5c8c9f6deee799b80fc3c11cb2d444b1ca84c6
javieresc97 <javieresc97@hotmail.com>
2018-05-03 22:03:19 -0500

    Empty array message

--- js/search.js
+++ js/search.js
@@ -2,53 +2,67 @@ $("#ageForm").submit(function (e) {
     e.preventDefault();
 
     var age = $("#ageInput").val();
+    $("#searchBtn").prop("disabled", true);
+    $("#searchBtn").html('Buscando...');
+
     var uri = 'https://query.wikidata.org/sparql?format=json&query=';
-    var query = 'SELECT distinct ?item ?birth ?birth_name ?death ?age WHERE {?item (wdt:P31/wdt:P279*) wd:Q5. ?item wdt:P1477 ?birth_name. ?item wdt:P569 ?birth. ?item wdt:P570 ?death. bind( year(?death)-year(?birth) as ?age )FILTER(?age=' + age + ')}LIMIT 100';
+    var query = 'SELECT distinct ?item ?birth_name ?birth ?death ?age WHERE {?item (wdt:P31/wdt:P279*) wd:Q5. ?item wdt:P1477 ?birth_name. ?item wdt:P569 ?birth. ?item wdt:P570 ?death. bind( year(?death)-year(?birth) as ?age )FILTER(?age=' + age + ')}LIMIT 200';
 
     var requestUri = uri + query;
-    
+
     $.ajax({
         type: 'GET',
         url: requestUri,
-        success: function (response){
-            var html = '<table border="1" id="customers">';
+        success: function (response) {
+            $("#searchBtn").prop("disabled", false);
+            $("#searchBtn").html('Buscar');
 
-            response.head.vars.forEach(element => {
-                html = html.concat("<th>");
-                html = html.concat(element);
-                html = html.concat("</th>");
-            });
+            if (response.results.bindings.length > 0) {
+                var html = '<table id="customers">';
 
-            response.results.bindings.forEach(element => {
-                html = html.concat("<tr>");
-                html = html.concat("<td>");
-                html = html.concat(element.item.value);
-                html = html.concat("</td>");
+                response.head.vars.forEach(element => {
+                    html = html.concat("<th>");
+                    html = html.concat(element);
+                    html = html.concat("</th>");
+                });
 
-                html = html.concat("<td>");
-                html = html.concat(element.birth_name.value);
-                html = html.concat("</td>");
+                response.results.bindings.forEach(element => {
+                    html = html.concat("<tr>");
+                    html = html.concat("<td>");
+                    html = html.concat(element.item.value);
+                    html = html.concat("</td>");
 
-                html = html.concat("<td>");
-                html = html.concat(element.birth.value);
-                html = html.concat("</td>");
+                    html = html.concat("<td>");
+                    html = html.concat(element.birth_name.value);
+                    html = html.concat("</td>");
 
-                html = html.concat("<td>");
-                html = html.concat(element.death.value);
-                html = html.concat("</td>");
+                    html = html.concat("<td>");
+                    html = html.concat(element.birth.value);
+                    html = html.concat("</td>");
 
-                html = html.concat("<td>");
-                html = html.concat(element.age.value);
-                html = html.concat("</td>");
-                html = html.concat("</tr>");
-            });
+                    html = html.concat("<td>");
+                    html = html.concat(element.death.value);
+                    html = html.concat("</td>");
 
-            html = html.concat("</table>");
-            $("#resultsDiv").html(html);
-            console.log(response);            
+                    html = html.concat("<td>");
+                    html = html.concat(element.age.value);
+                    html = html.concat("</td>");
+                    html = html.concat("</tr>");
+                });
+
+                html = html.concat("</table>");
+                $("#resultsDiv").html(html);
+            }
+            else
+            {
+                var html = "<label>No se encontraron resultados</label>";
+                $("#resultsDiv").html(html);
+            }
         },
-        error: function (a,b,c){
-            console.log(a);            
+        error: function (a, b, c) {
+            $("#searchBtn").prop("disabled", false);
+            $("#searchBtn").html('Buscar');
+            console.log(a);
         }
     });
 });
